67c33fbb91be5b76a50d8fb078c29a7583204519
ILoveGarlicBread <therecipeofholywater@gmail.com>
2023-12-12 08:44:26 +0700

    Changes to be committed: 	modified:   copy_dotfiles.sh

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-echo"Copying .zshrc to /home/"$USER
-cp .zshrc /home/$USER/
+cd
+echo "Copying .zshrc"
+if [ -d ~/ ]; then
+  cp ~/dotfiles/.zshrc ~/
+fi 
+
+
+if [ -d ~/.config/kitty/]; then
+  cp ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/
+  cp ~/dotfiles/.config/kitty/nord.conf ~/.config/kitty/
+  cp ~/dotfiles/.config/kitty/everforest.conf ~/.config/kitty/
+  cp ~/dotfiles/.config/kitty/gruvbox_dark.conf ~/.config/kitty/
+else 
+  mkdir ~/.config/kitty/
+  cp ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/
+  cp ~/dotfiles/.config/kitty/nord.conf ~/.config/kitty/
+  cp ~/dotfiles/.config/kitty/everforest.conf ~/.config/kitty/
+  cp ~/dotfiles/.config/kitty/gruvbox_dark.conf ~/.config/kitty/
+fi
+  
+
+
+
+
+
