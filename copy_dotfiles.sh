ed04f2908b2cbda0b7ae56fd9a7c39b52d5a4630
ILoveGarlicBread <therecipeofholywater@gmail.com>
2023-12-13 00:02:27 +0700

    Changes to be committed: 	modified:   copy_dotfiles.sh

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -2,9 +2,10 @@
 cd
 source_folder="~/dotfiles/"
 destination_folder="~/.config/"
-echo "Moving .zshrc"
+echo "Moving .zshrc..."
 cp ~/dotfiles/.zshrc ~/
-
+if [ %? -eq 0]; then
+  echo ".zshrc moved successfully."
 
 echo "Moving kitty"
 mkdir -p ~/.config/kitty/
@@ -12,11 +13,16 @@ cp ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/
 cp ~/dotfiles/.config/kitty/nord.conf ~/.config/kitty/
 cp ~/dotfiles/.config/kitty/everforest.conf ~/.config/kitty/
 cp ~/dotfiles/.config/kitty/gruvbox_dark.conf ~/.config/kitty/
+if [ %? -eq 0]; then
+  echo ".zshrc moved successfully."
+
 
 echo "Moving fastfetch"
 mkdir -p ~/.config/fastfetch/
 cp ~/dotfiles/.config/fastfetch/config.conf ~/.config/fastfetch/
- 
+if [ %? -eq 0]; then
+  echo ".zshrc moved successfully."
+
   
 
 
