94514f6b20e8faff2ef405fbf86695765e126d6e
ILoveGarlicBread <therecipeofholywater@gmail.com>
2024-01-19 22:23:54 +0700

    modified:   .config/fastfetch/config.jsonc 	deleted:    .config/kmonad/config.kbd 	modified:   .zshrc 	modified:   backup.sh 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Post-install set up script
+# WARNING:
+# - This script is designed to be used after a fresh install of Endeavour OS with Gnome desktop.
+# - This is my very first bash script, certain things may not work as expected.
 
 cd
 
@@ -12,60 +15,63 @@ function main() {
 	gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 	echo "Tap to click status: "
 	gsettings get org.gnome.desktop.peripherals.touchpad tap-to-click
-	echo""
-	echo "Adjusting volume step..."
+
+	echo "\nAdjusting volume step..."
 	gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 1
 	echo -n "Volume step = "
 	gsettings get org.gnome.settings-daemon.plugins.media-keys volume-step
-	echo""
-
-	# Install gnome extensions
-	#
 
 	# Move config files
-	echo "Moving .zshrc..."
+	echo "\nMoving .zshrc..."
 	cp ~/dotfiles/.zshrc ~/
-	if [ $? -eq 0 ]; then
-		echo ".zshrc moved successfully."
-	fi
-	echo""
 
-	echo "Moving kitty..."
+	echo "\nMoving kitty..."
 	mkdir -p ~/.config/kitty/
 	cp -r ~/dotfiles/.config/kitty ~/.config/
-	if [ $? -eq 0 ]; then
-		echo "kitty config moved successfully."
-	fi
-	echo""
 
-	echo "Moving fastfetch..."
+	echo "\nMoving fastfetch..."
 	mkdir -p ~/.config/fastfetch/
 	cp ~/dotfiles/.config/fastfetch/config.conf ~/.config/fastfetch/
-	if [ $? -eq 0 ]; then
-		echo "fastfetch config moved successfully."
-	fi
-	echo""
 
-	echo "Applying fonts..."
+	echo "\nApplying fonts..."
 	mkdir -p ~/.local/share/fonts/
 	cp -r ~/dotfiles/fonts ~/.local/share/
-
+	# Set fonts
 	gsettings set org.gnome.desktop.interface document-font-name 'Roboto 11'
 	gsettings set org.gnome.desktop.interface monospace-font-name 'FiraCode Nerd Font Mono weight=450 10'
 	gsettings set org.gnome.desktop.interface font-name 'Roboto 11'
 	gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Roboto Bold 11'
-	if [ $? -eq 0 ]; then
-		echo "Fonts applied successfully"
-	fi
-	echo""
 
-	echo "Applying theme..."
-	mkdir -p ~/.themes
-	cp -r ~/dotfiles/.themes/Colloid-Dark-Nord ~/.theme/
+	echo "\nApplying cursor"
+	cd
+	git clone https://github.com/vinceliuice/Qogir-icon-theme.git
+	cp -r ~/Qogir-icon-theme/src/cursors/dist-ubuntu-dark ~/.local/share/icons/Qogir-ubuntu-white-cursors
+	gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-ubuntu-white-cursors'
+	rm -rf ~/Qogir-icon-theme
+
+	echo "\nApplying icon theme"
+	cd
+	git clone https://github.com/alvatip/Nordzy-icon
+	cd Nordzy-icon
+	./install.sh -t yellow -c dark
+	cd
+	gsettings set org.gnome.desktop.interface icon-theme 'Nordzy-yellow-dark'
+	rm -rf ~/Nordzy-icon
+
+	echo "\nApplying gnome theme ..."
+	git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+	cd Colloid-gtk-theme
+	./install.sh --tweaks nord rimless float
+	cd
 	gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Dark-Nord"
+	gsettings set org.gnome.desktop.interface gtk-theme 'Colloid-Dark-Nord'
+	rm -rf ~/Colloid-gtk-theme
 
-	echo "Applying kmonad config"
+	echo "\nApplying kmonad config"
 	mkdir -p ~/.config/kmonad
+	cp -r ~/dotfiles/kmonad ~/.config/kmonad/
+	mkdir -p ~/.config/autostart
+	cp ~/dotfiles/autostart/kmonad.desktop ~/.config/autostart/
 
 	# Installing packages
 	packages=(
@@ -104,13 +110,21 @@ function main() {
 	for package in "${packages[@]}"; do
 		yay -S --noconfirm "$package"
 	done
+
+	# Setup zsh shell
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	cd
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+	git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+	git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+
+	# Setup neovim
 	rm -rf ~/.config/nvim
-	git clone git@github.com:ILoveGarlicBread/neovim.git ~/.config/neovim
+	mkdir -p ~/.config/nvim
+	git clone https://github.com/ILoveGarlicBread/neovim.git ~/.config/nvim/
+
 	# Install gnome extensions and apply config
 	cd ~/dotfiles/
 	#./install-gnome-extensions.sh --enable --file links.txt
