99ae2079a3c338c39f5e9bc63d97d43758b8fa67
lam <therecipeofholywater@gmail.com>
2024-04-02 08:22:17 +0700

    added .tmux.conf

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@
 cp -r ~/.config/kitty ~/dotfiles/.config
 cp -r ~/.config/fastfetch ~/dotfiles/.config
 cp -r ~/.config/kmonad ~/dotfiles/.config
-cp ~/.config/autostart/kmonad.desktop ~/dotfiles/.config/autostart
+cp ~/.tmux.conf ~/dotfiles
 dconf dump /org/gnome/shell/extensions/ >~/dotfiles/extensions.conf
