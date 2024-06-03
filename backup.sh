96b79a1dbe7371134ab9964c83ac703f5ca5e326
lam <therecipeofholywater@gmail.com>
2024-06-03 21:25:52 +0700

    Added alacritty config

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@
 cp ~/.zshrc ~/dotfiles
 cp ~/.tmux/plugins/tmux/themes/catppuccin_mocha.tmuxtheme ~/dotfiles
 cp -r ~/.config/kitty ~/dotfiles/.config
+cp -r ~/.config/alacritty/ ~/dotfiles/.config
 cp -r ~/.config/fastfetch ~/dotfiles/.config
 cp -r ~/.config/kmonad ~/dotfiles/.config
 cp ~/.tmux.conf ~/dotfiles
