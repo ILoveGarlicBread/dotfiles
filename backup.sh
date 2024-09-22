c49b30c0beb1f177ebbb3b15ad691c3f92c76ba8
lam <therecipeofholywater@gmail.com>
2024-09-22 09:46:27 +0700

    Add vi-mode

--- backup.sh
+++ backup.sh
@@ -3,7 +3,6 @@
 cp ~/.zshrc ~/dotfiles
 cp ~/.tmux/plugins/tmux/themes/catppuccin_mocha.tmuxtheme ~/dotfiles
 cp -r ~/.config/kitty ~/dotfiles/.config
-cp -r ~/.config/alacritty/ ~/dotfiles/.config
 cp -r ~/.config/fastfetch ~/dotfiles/.config
 cp -r ~/.config/kmonad ~/dotfiles/.config
 cp ~/.tmux.conf ~/dotfiles
